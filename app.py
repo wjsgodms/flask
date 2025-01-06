d403295ef05ee9ab317626580580fe0ed50678b1
전해은 <ggoo7731@naver.com>
2025-01-06 13:03:20 +0900

    flask 과제

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+users = [
+    {"username": "traveler", "name": "Alex"},
+    {"username": "photographer", "name": "Sam"},
+    {"username": "gourmet", "name": "Chris"}
+]
+@app.route('/')
+def index():
+    return render_template('index.html', users=users)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
