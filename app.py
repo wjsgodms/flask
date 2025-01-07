2b63299fa50df0c8bb8967ddb529b33c26c62412
jhe <ggoo7731@naver.com>
2025-01-07 10:12:46 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_smorest import Api
+from api import book_blp
 
 app = Flask(__name__)
 
-users = [
-    {"username": "traveler", "name": "Alex"},
-    {"username": "photographer", "name": "Sam"},
-    {"username": "gourmet", "name": "Chris"}
-]
-@app.route('/')
-def index():
-    return render_template('index.html', users=users)
+app.config['API_TITLE'] = 'Book API'
+app.config['API_VERSION'] = 'v1'
+app.config['OPENAPI_VERSION'] = '3.0.2'
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+
+api = Api(app)
+api.register_blueprint(book_blp)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
